bf686b5086dd06709c1b0759528b914a4979201b
kimnamlhn <kimnam.cpc@gmail.com>
2021-08-12 16:57:10 +0700

    todo list ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ActivitiesComponent } from './components/activities/activities.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivitiesComponent
   ],
   imports: [
     BrowserModule
