8e5b94e8ef8d0a4aafef06f9fc060bb7fd8779e8
kimnamlhn <kimnam.cpc@gmail.com>
2021-08-12 21:46:12 +0700

    deleteactivity feature

--- src/app/components/activities/activities.component.ts
+++ src/app/components/activities/activities.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-activities',
   templateUrl: './activities.component.html',
   styleUrls: ['./activities.component.css']
 })
 export class ActivitiesComponent implements OnInit {
-
-  constructor() { }
+  constructor() {   }
+  activities: Array<string> = [];
 
   ngOnInit(): void {
+    this.activities = [
+      'Xem phim', 'Hoc bai'
+    ]
+
+  
   }
+
+  deleteActivity (id:number) {
+    //gan activities bang activities moi sau khi loai di activities[id]
+    this.activities = this.activities.filter((value,i) => i !== id);
+  }
+
 
 }
